1f1416aedc48d91fdc34b2e5f4787a5464050290
Abhishek Shingade <abhishekshingade@zoho.com>
2019-07-12 09:19:39 +0530

    Add files via upload

---
+++ JobSurfer_Solution/JobSurfer/W_MasterPage/Master_Site_1.Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JobSurfer.W_MasterPage
+{
+    public partial class Master_Site_1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TA_1_Tick(object sender, EventArgs e)
+        {
+            LblA_1.Text = DateTime.Now.ToString("F");
+        }
+    }
+}
